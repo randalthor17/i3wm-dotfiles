cefe7d5b0824c18d1a3aa116d9eabc9d3e8b93d6
Rand al'Thor <83530912+randalthor17@users.noreply.github.com>
2022-01-03 10:59:56 +0600

    Update install.sh

--- pkglist/install.sh
+++ pkglist/install.sh
@@ -4,7 +4,7 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-cat chaotic-aur.txt | sudo tee -a /etc/pacman.d/mirrorlist > /dev/null
+cat chaotic-aur.txt | sudo tee -a /etc/pacman.conf > /dev/null
 sudo pacman -S --needed - <  native.txt
 yay -S --needed - < aur.txt
 echo "Done!"
